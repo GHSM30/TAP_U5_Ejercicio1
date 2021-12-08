fe52521417b8464c9a1fd0daa7a00e6e090197f9
Guillermo Hilario Santos Moya <guhisantosmo@ittepic.edu.mx>
2021-12-08 11:32:06 -0700

    Ejercicio  1 U5 TAP

---
+++ app/src/main/java/com/example/tap_u5_ejercicio1/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.tap_u5_ejercicio1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)  //initComponet()
+
+
+
+
+    }
+}
